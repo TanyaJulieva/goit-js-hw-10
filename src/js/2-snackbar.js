572ff479c3cf3429a9a5a6e96a08b8e43bfe210f
TanyaJulieva <128372881+TanyaJulieva@users.noreply.github.com>
2024-02-17 14:04:30 +0100

    upd snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,25 +2,32 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const elements = {
+  form: document.querySelector('.form'),
   submitBtn: document.querySelector('[type="submit"]'),
   fulfilledInput: document.querySelector('[value="fulfilled"]'),
   rejectedInput: document.querySelector('[value="rejected"]'),
   delayInput: document.querySelector('[name="delay"]'),
 };
 
-elements.submitBtn.addEventListener('submit', handlerSubmit);
+elements.form.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(evt) {
   evt.preventDefault();
 
-  let delay = Number(elements.delayInput.value);
-  console.log(delay);
+  // elements.fulfilledInput.checked = false;
+  // elements.rejectedInput.checked = false;
+  // elements.delayInput.value = '';
+
+  let delay = elements.delayInput.value;
 
   const promise = new Promise((res, rej) => {
+    elements.delayInput.value = '';
+
     setTimeout(() => {
       if (elements.fulfilledInput.checked) {
         res(delay);
-      } else {
+      }
+      if (elements.rejectedInput.checked) {
         rej(delay);
       }
       console.log(promise);
@@ -28,14 +35,17 @@ function handlerSubmit(evt) {
   });
 
   promise
-  .then((delay) =>
-    iziToast.show({
-      message: `✅ Fulfilled promise in ${delay}ms`,
+    .then(delay => {
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
     })
-  )
+    .catch(delay => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
 
-  .catch((delay) => iziToast.show({
-    message: `✅ Fulfilled promise in ${delay}ms`,
-  }));
-  
+  // elements.fulfilledInput.checked = false;
+  // elements.rejectedInput.checked = false;
 }
