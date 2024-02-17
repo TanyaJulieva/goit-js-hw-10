21c00c78dcdbb8015232d985f1db28bf1e988e15
TanyaJulieva <128372881+TanyaJulieva@users.noreply.github.com>
2024-02-17 18:42:36 +0100

    icons

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,5 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import Icon from '/img/icons.svg';
+
 
 const elements = {
   form: document.querySelector('.form'),
@@ -14,31 +16,44 @@ function handlerSubmit(evt) {
   evt.preventDefault();
 
   let delay = elements.delayInput.value;
+  createPromise(delay)
 
-  const promise = new Promise((res, rej) => {
-    setTimeout(() => {
-      if (elements.fulfilledInput.checked) {
-        res(delay);
-      }
-      if (elements.rejectedInput.checked) {
-        rej(delay);
-      }
-      console.log(promise);
-    }, delay);
-  });
 
-  promise
-    .then(delay => {
-      iziToast.show({
-        message: `Fulfilled promise in ${delay}ms`,
-      });
-    })
-    .catch(delay => {
-      iziToast.show({
-        icon: '',
-        message: `Rejected promise in ${delay}ms`,
-      });
+  function createPromise(delay) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        if (elements.fulfilledInput.checked) {
+          res(delay);
+        }
+        if (elements.rejectedInput.checked) {
+          rej(delay);
+        }
+        console.log(promise);
+      }, delay);
     });
+  
+    
+      .then(delay => {
+        iziToast.show({
+          iconUrl: Icon,
+          title: 'OK',
+          titleColor: '#FFFFFF',
+          backgroundColor: '#59A10D',
+          messageColor: '#FFFFFF',
+          message: `Fulfilled promise in ${delay}ms`,
+        });
+      })
+      .catch(delay => {
+        iziToast.show({
+          title: 'Error',
+          titleColor: '#FFFFFF',
+          backgroundColor: '#EF4040',
+          messageColor: '#FFFFFF',
+          message: `Rejected promise in ${delay}ms`,
+        });
+      });
+  }
+    
 
     evt.currentTarget.reset()
 }
